9009c7ad4527e8197b95febb76323a2d23156ddb
asep.komarudin <asep.komarudin@dataon.com>
2024-05-30 06:53:57 +0700

    add: custom color lsp property

--- lua/darcula.lua
+++ lua/darcula.lua
@@ -626,6 +626,10 @@ M.configure_highlights = function()
   hi(0, "@tag.delimiter", { fg = color._58 })
   hi(0, "@variable.member.javascript", { fg = color.lavender })
   hi(0, "@property.json", { fg = color.lavender })
+  hi(0, "@lsp.type.property", { fg = color.lavender })
+  hi(0, "NoiceCmdlinePopupBorder", { fg = color.medium_gray })
+  hi(0, "NoiceCmdlinePopupBorderSearch", { fg = color.pale_taupe })
+
   -- Terminal colors
   local g = vim.g
 
@@ -645,49 +649,6 @@ M.configure_highlights = function()
   g.terminal_color_13 = color.pale_gold
   g.terminal_color_14 = color.bright_sky
   g.terminal_color_15 = color.very_light_gray
-
-  -- custom lualine
-  --   local theme = {
-  --     normal = {
-  --       a = { fg = color.silver_gray, bg = color.medium_gray },
-  --       b = { fg = color.silver_gray, bg = color.background },
-  --       c = { fg = color.light_slate_gray, bg = "NONE" },
-  --     },
-  --
-  --     insert = {
-  --       a = { fg = color.background, bg = color.green },
-  --       b = { fg = color.green, bg = color.background },
-  --     },
-  --     visual = {
-  --       a = { fg = color.background, bg = color.bright_cyan },
-  --       b = { fg = color.bright_cyan, bg = color.background },
-  --     },
-  --     replace = {
-  --       a = { fg = color.background, bg = color.red },
-  --       b = { fg = color.red, bg = color.background },
-  --     },
-  --     command = {
-  --       a = { fg = color.background, bg = color.burnt_orange },
-  --       b = { fg = color.burnt_orange, bg = color.background },
-  --     },
-  --     terminal = {
-  --       a = { fg = color.background, bg = color.burnt_orange },
-  --       b = { fg = color.burnt_orange, bg = color.background },
-  --     },
-  --     inactive = {
-  --       a = { fg = color.silver_gray, bg = "NONE" },
-  --       b = { fg = color.silver_gray, bg = "NONE" },
-  --       c = { fg = color.silver_gray, bg = "NONE" },
-  --     },
-  --   }
-  --   local ok, lualine = pcall(require, "lualine")
-  --   if ok then
-  --     lualine.setup({
-  --       options = {
-  --         theme = theme,
-  --       },
-  --     })
-  --   end
 end
 
 return M
